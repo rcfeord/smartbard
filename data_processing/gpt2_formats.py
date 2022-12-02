98f34c35fd14730debea542abf1d4d77acfb4b63
RascusMoro <russo.gmarco@gmail.com>
2022-12-02 15:16:54 +0100

    Rewritten limerick_add_special_tkns to add <RA>, <RB> tokens

--- data_processing/gpt2_formats.py
+++ data_processing/gpt2_formats.py
@@ -17,6 +17,55 @@ def load_encodings() -> dict:
     return encodings_dict
 
 
+def tokenize_rhymes_ab(verse: str, line_number: int) -> str:
+    """ add <RA> and <RB> tokens to the final word of a verse"""
+    # example: "Should swallow a hand-grenade!"
+
+    # split verse => ['Should', 'swallow', 'a', 'hand-grenade!']
+    token_list = verse.split()
+    # get last piece of the verse => 'hand-grenade!'
+    last_index = max(loc for loc, val in enumerate(token_list) if re.search('[a-zA-Z]', val))
+    last_token = token_list[last_index]
+    # get code based on line number => <RA>
+    a_lines = (0,1,4)
+    b_lines = (2,3)
+    if line_number in a_lines:
+        code = '<RA>'
+    elif line_number in b_lines:
+        code = '<RB>'
+
+    # add code before last token => ['Should', 'swallow', 'a', '<RA>', 'hand-grenade!']
+    token_list_reversed = token_list[::-1]
+    pos = token_list_reversed.index(last_token)
+    token_list_reversed.insert(pos + 1, code)
+
+    # return tokenized verse as a string => "Should swallow a <RA> hand-grenade!"
+    return ' '.join(token_list_reversed[::-1])
+
+
+def tokenize_rhymes_with_code(verse: str, encodings: dict) -> str:
+    """ add <n> tokens to the final word of a verse"""
+    # example: "Should swallow a hand-grenade!"
+
+    # split verse => ['Should', 'swallow', 'a', 'hand-grenade!']
+    token_list = verse.split()
+    # get last piece of the verse => 'hand-grenade!'
+    last_index = max(loc for loc, val in enumerate(token_list) if re.search('[a-zA-Z]', val))
+    last_token = token_list[last_index]
+    # extract last real word (containing only alphabetic characters) => 'grenade'
+    last_word = [token for token in re.split('[^a-zA-Z]', last_token) if token.isalpha()][-1]
+    # get code number for its ending sounds => 8
+    code = encodings.get(last_word, -1)
+
+    # add code before last token => ['Should', 'swallow', 'a', '<8>', 'hand-grenade!']
+    token_list_reversed = token_list[::-1]
+    pos = token_list_reversed.index(last_token)
+    token_list_reversed.insert(pos + 1, f'<{code}>')
+
+    # return tokenized verse as a string => "Should swallow a <8> hand-grenade!"
+    return ' '.join(token_list_reversed[::-1])
+
+
 def limerick_add_special_tkns(df: pd.DataFrame) -> pd.DataFrame:
     """
     add special tokens to the limerick:
@@ -26,28 +75,6 @@ def limerick_add_special_tkns(df: pd.DataFrame) -> pd.DataFrame:
     <LE> at the end of each line,
     <n> before every final word on each line, where <n> represents the ending sounds.
     """
-    def tokenize_rhymes(verse: str) -> str:
-        """ add <n> tokens to the final word of a verse"""
-        # example: "Should swallow a hand-grenade!"
-
-        # split verse => ['Should', 'swallow', 'a', 'hand-grenade!']
-        token_list = verse.split()
-        # get last piece of the verse => 'hand-grenade!'
-        last_index = max(loc for loc, val in enumerate(token_list) if re.search('[a-zA-Z]', val))
-        last_token = token_list[last_index]
-        # extract last real word (containing only alphabetic characters) => 'grenade'
-        last_word = [token for token in re.split('[^a-zA-Z]', last_token) if token.isalpha()][-1]
-        # get code number for its ending sounds => 8
-        code = encodings.get(last_word, -1)
-
-        # add code before last token => ['Should', 'swallow', 'a', '<8>', 'hand-grenade!']
-        token_list_reversed = token_list[::-1]
-        pos = token_list_reversed.index(last_token)
-        token_list_reversed.insert(pos + 1, f'<{code}>')
-
-        # return tokenized verse as a string => "Should swallow a <8> hand-grenade!"
-        return ' '.join(token_list_reversed[::-1])
-
     # load encodings from json file to a dict
     encodings = load_encodings()
     # create 'limerick start' and 'limerick end' special tokens
@@ -72,8 +99,10 @@ def limerick_add_special_tkns(df: pd.DataFrame) -> pd.DataFrame:
             start_tkn = '<LA> ' if index in (0, 1, 4) else '<LB> ' # token at the start of line
             # end_tkn = ' <LE>' # token at the end of line
 
-            # add rhyme tokens to the last words
-            verse = tokenize_rhymes(verse)
+            # ALTERNATIVE_1: add rhyme tokens to the last words based on endings
+            # verse = tokenize_rhymes_with_code(verse, encodings=encodings)
+            # ALTERNATIVE_2: add rhyme tokens to the last words based on line num
+            verse = tokenize_rhymes_ab(verse, line_number=index)
             # append tokenized verse to temp list
             list_temp.append(start_tkn + verse)# + end_tkn)
 
